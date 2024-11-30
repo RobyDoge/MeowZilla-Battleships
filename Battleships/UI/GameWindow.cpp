6620981f4d434707b014752f07793b9c38b8ebe2
Rosca Silviu <silviu.rosca@student.unitbv.ro>
2024-11-30 13:13:57 +0200

    small change

--- Battleships/UI/GameWindow.cpp
+++ Battleships/UI/GameWindow.cpp
@@ -19,13 +19,10 @@ GameWindow::GameWindow(std::vector<Ship*> ships,QWidget *parent)
     enemyBoard = new EnemyBoard(this);
     splitter->addWidget(enemyBoard);
 
-    QScreen* screen = QGuiApplication::primaryScreen();
-    QSize size = screen->size();
-
+    
     // Creează board-ul (tabla ta)
     PlayerBoard* board = new PlayerBoard(this);
-    //board->setFixedSize(720, 720);
-    board->setFixedSize(size.width() / 2, size.height());
+    board->setFixedSize(720, 720);
     board->setShips(ships);
     splitter->addWidget(board);
 
